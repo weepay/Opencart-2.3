ba96cfde9667a2d735e2cd515fea12677cb03bbc
weepay <49822776+weepay@users.noreply.github.com>
2019-09-26 12:31:37 +0300

    Update weepay_payment.php

--- admin/controller/extension/payment/weepay_payment.php
+++ admin/controller/extension/payment/weepay_payment.php
@@ -93,7 +93,7 @@ class ControllerExtensionPaymentWeepayPayment extends Controller
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('text_home'),
-            'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+            'href' => $this->url->link('common/home', 'token=' . $this->session->data['token'], true),
             'separator' => false,
         );
 
@@ -105,14 +105,14 @@ class ControllerExtensionPaymentWeepayPayment extends Controller
 
         $data['breadcrumbs'][] = array(
             'text' => $this->language->get('heading_title'),
-            'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', 'SSL'),
+            'href' => $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true),
 
             'separator' => ' :: ',
         );
 
-        $data['action'] = $this->url->link('extension/payment/weepay_payment', 'token=' . $this->session->data['token'], 'SSL');
+        $data['action'] = $this->url->link('extension/payment/weepay_payment', 'token=' . $this->session->data['token'], true);
 
-        $data['cancel'] = $this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL');
+        $data['cancel'] = $this->url->link('extension/extension', 'token=' . $this->session->data['token'] . '&type=payment', true);
 
         $merchant_keys_name_array = array(
             'weepay_payment_bayiid',
@@ -183,7 +183,7 @@ class ControllerExtensionPaymentWeepayPayment extends Controller
             unset($this->session->data['weepay_update']);
             $this->load->controller('extension/modification/refresh');
             $this->session->data['success'] = $this->language->get('text_success');
-            $this->response->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], 'SSL'));
+            $this->response->redirect($this->url->link('extension/payment', 'token=' . $this->session->data['token'], true));
         } else {
             $this->response->redirect($this->url->link('extension/payment/weepay_payment', 'token=' . $this->session->data['token'] . "&update_error=$updated", true));
         }
